836b17459afdc0b0c3de168685380325f33267ba
Amy Phung <phung.amy85@gmail.com>
2019-04-29 16:13:40 -0700

    write destroy test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -58,5 +58,17 @@ describe UsersController do
     end
   end
   
-  
+  describe "destroy" do
+    it "ends a user's session" do
+      user = users(:grace)
+      perform_login(user)
+      
+      delete logout_path
+    
+      
+      expect(session[:user_id]).must_be_nil
+      
+      must_redirect_to root_path
+    end
+  end
 end
